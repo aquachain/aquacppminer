f92cafa720b1e88425b0d99948c71e2cc95b2973
cryptogone <cryptogone.dev@gmail.com>
2018-05-31 13:16:39 +0200

    logLine helper with std:string prefix

--- src/log.h
+++ src/log.h
@@ -3,4 +3,4 @@
 #include <stdarg.h>
 
 void logLine(const char* prefix, const char* fmt, ...);
-void logNewLine(const char* prefix, const char* fmt, ...);
+void logLine(std::string prefix, const char* fmt, ...);
