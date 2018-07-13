e809f2c4cdc24d57a2b2f8f601f6679c36e08480
cryptogone <cryptogone.dev@gmail.com>
2018-07-13 18:44:21 +0200

    improve -h output

--- src/args.cpp
+++ src/args.cpp
@@ -13,20 +13,18 @@ const std::string OPT_REFRESH_RATE = "-r";
 const std::string OPT_SOLO = "--solo";
 
 static const std::string s_usageMsg = 
-"--- Usage ---\n"
-"aquacppminer.exe -F url [-t nThreads] [-n nodeUrl] [--solo] [-h]\n"
-"  -F url              : url of pool or node to mine on, if not specified, will pool mine to dev's aquabase\n"
-"  --solo              : solo mining, -F needs to be the node url\n"
-"  -t nThreads         : number of threads to use (if not specified will use maximum logical threads available)\n"
-"  -n node_url         : optional node url, to get more stats (pool mining only)"
-"  -r                  : refresh rate, ex: 1s, 2.5m\n"
-"  -h                  : display this help message and exit\n"
-"\n"
+"aquacppminer.exe -F url [-t nThreads] [-n nodeUrl] [--solo] [-r refreshRate] [-h]\n"
+"  -F url        : url of pool or node to mine on, if not specified, will pool mine to dev's aquabase\n"
+"  -t nThreads   : number of threads to use (if not specified will use maximum logical threads available)\n"
+"  -n node_url   : optional node url, to get more stats (pool mining only)\n"
+"  -r rate       : pool refresh rate, ex: 1s, 2.5m, default is 1s\n"
+"  --solo        : solo mining, -F needs to be the node url\n"
+"  -h            : display this help message and exit\n"
 ;
 
 void printUsage()
 {
-	printf("%s\n", s_usageMsg.c_str());
+	printf("\n%s\n", s_usageMsg.c_str());
 }
 
 std::pair<bool, uint32_t> parseRefreshRate(const std::string& refreshRateStr) {
